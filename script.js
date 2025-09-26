df2d3994207d8b705dc1999d6f76ab827dc16fa0
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-26 08:14:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+const form=document.getElementById("MyForm");
 function volume_sphere() {
-    //Write your code here
-  
+  let radius=Number(form[0].value);
+  radius=Math.pow(radius,3);
+  let volume=(radius*4)/3;
+  volume*=3.14;
+  form[1].value=volume;
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
